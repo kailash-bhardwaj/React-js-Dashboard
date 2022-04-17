4ccb31a58f78757b892a518ffd56493b802c197a
Kailash <kailash.bhardwaj49@gmail.com>
2022-04-17 23:38:05 +0530

    test

--- src/pages/auth/Auth.js
+++ src/pages/auth/Auth.js
@@ -1,26 +1,38 @@
-
 import Login from './login/Login';
 import React from 'react';
 import ForgotPassword from './forgotPassword/ForgotPassword';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ReactDOM from 'react-dom';
+
 function Auth() {
-    ReactDOM.render((
-  <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Login />
-          </Route>
-          <Route path="/forgot">
-            <ForgotPassword />
-          </Route>
-         
-        </Switch>
-      </BrowserRouter>
-     
-     ), document.getElementById('root'));
+    ReactDOM.render(( <
+        div ClassName = "test" >
+        <
+        BrowserRouter >
+        <
+        Switch >
+        <
+        Route exact path = "/" >
+        <
+        Login / >
+        <
+        /Route> <
+        Route path = "/forgot" >
+        <
+        ForgotPassword / >
+        <
+        /Route>
+
+        <
+        /Switch> <
+        /BrowserRouter> <
+        p > test < /p> <
+        /div>
+
+
+
+    ), document.getElementById('root'));
 
 }
 
 export default Auth;
-
